21caee6565de28c330096055c594ad1608e2873c
nobu-g <ueda-n127@hotmail.co.jp>
2021-09-18 20:16:25 +0900

    add test_morpheme_to_jumanpp

--- tests/units/test_morpheme.py
+++ tests/units/test_morpheme.py
@@ -2,30 +2,42 @@ from dataclasses import astuple, dataclass
 
 import pytest
 
-from rhoknp.units import Document
+from rhoknp.units import Morpheme
 
 
 @dataclass
 class MorphemeTestCase:
     jumanpp: str
-    texts: list[str]
+    text: str
 
 
 cases = [
     MorphemeTestCase(
-        jumanpp="""外国 がいこく 外国 名詞 6 普通名詞 1 * 0 * 0 "代表表記:外国/がいこく ドメイン:政治 カテゴリ:場所-その他"
-人 じん 人 名詞 6 普通名詞 1 * 0 * 0 "代表表記:人/じん カテゴリ:人 漢字読み:音"
-参政 さんせい 参政 名詞 6 サ変名詞 2 * 0 * 0 "代表表記:参政/さんせい ドメイン:政治 カテゴリ:抽象物"
-権 けん 権 名詞 6 普通名詞 1 * 0 * 0 "代表表記:権/けん カテゴリ:抽象物 漢字読み:音"
-EOS""",
-        texts=["外国", "人", "参政", "権"],
-    )
+        jumanpp='外国 がいこく 外国 名詞 6 普通名詞 1 * 0 * 0 "代表表記:外国/がいこく ドメイン:政治 カテゴリ:場所-その他"',
+        text="外国",
+    ),
+    MorphemeTestCase(
+        jumanpp='人 じん 人 名詞 6 普通名詞 1 * 0 * 0 "代表表記:人/じん カテゴリ:人 漢字読み:音"',
+        text="人",
+    ),
+    MorphemeTestCase(
+        jumanpp='参政 さんせい 参政 名詞 6 サ変名詞 2 * 0 * 0 "代表表記:参政/さんせい ドメイン:政治 カテゴリ:抽象物"',
+        text="参政",
+    ),
+    MorphemeTestCase(
+        jumanpp='権 けん 権 名詞 6 普通名詞 1 * 0 * 0 "代表表記:権/けん カテゴリ:抽象物 漢字読み:音"',
+        text="権",
+    ),
 ]
 
 
+@pytest.mark.parametrize("jumanpp,text", [astuple(case) for case in cases])
+def test_morpheme_from_jumanpp(jumanpp: str, text: str):
+    morpheme = Morpheme.from_jumanpp(jumanpp)
+    assert morpheme.text == text
+
+
 @pytest.mark.parametrize("jumanpp,texts", [astuple(case) for case in cases])
-def test_morpheme(jumanpp: str, texts: list[str]):
-    document = Document.from_jumanpp(jumanpp)
-    for i, morpheme in enumerate(document.morphemes):
-        assert i == morpheme.index
-        assert texts[i] == morpheme.text
+def test_morpheme_to_jumanpp(jumanpp: str, texts: list[str]):
+    morpheme = Morpheme.from_jumanpp(jumanpp)
+    assert morpheme.to_jumanpp() == jumanpp
