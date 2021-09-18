24d59c6348c16f07414f8160ab876805638597aa
nobu-g <ueda-n127@hotmail.co.jp>
2021-09-18 16:47:41 +0900

    add Features

---
+++ src/rhoknp/utils/features.py
@@ -0,0 +1,34 @@
+import re
+from typing import Union
+
+
+class Features(dict):
+    """A class to represent a feature information for a chunk or a phrase
+
+    This class parses tags in features string and converts to a dictionary.
+    ex. "<正規化代表表記:遅れる/おくれる>" --> {"正規化代表表記": "遅れる/おくれる"}
+    """
+    KNP_PATTERN: re.Pattern = re.compile(r"<(?P<key>[^:]+?)(:(?P<value>.+?))?>")
+
+    def __init__(self, fstring: str):
+        super().__init__()
+        for match in self.KNP_PATTERN.finditer(fstring):
+            self[match.group("key")] = match.group("value") or True
+
+    @classmethod
+    def from_fstring(cls, fstring) -> "Features":
+        return cls(fstring)
+
+    def to_fstring(self) -> str:
+        return "".join(self._item2tag_string(k, v) for k, v in self.items())
+
+    @staticmethod
+    def _item2tag_string(key: str, value: Union[str, bool]) -> str:
+        if value is False:
+            return ""
+        if value is True:
+            return f"<{key}>"
+        return f"<{key}:{value}>"
+
+    def __str__(self) -> str:
+        return self.to_fstring()
---
+++ tests/utils/test_features.py
@@ -0,0 +1,37 @@
+from dataclasses import astuple, dataclass
+from typing import Union
+
+import pytest
+
+from rhoknp.utils.features import Features
+
+
+@dataclass(frozen=True)
+class FeaturesTestCase:
+    fstring: str
+    features: dict[str, Union[str, bool]]
+    length: int
+
+
+cases = [
+    FeaturesTestCase(
+        fstring="""<BGH:構文/こうぶん><文節内><係:文節内><文頭><体言><名詞項候補><先行詞候補><正規化代表表記:構文/こうぶん>""",
+        features={"BGH": "構文/こうぶん", "文節内": True, "係": "文節内", "文頭": True, "体言": True, "名詞項候補": True,
+                  "先行詞候補": True, "正規化代表表記": "構文/こうぶん"},
+        length=8,
+    )
+]
+
+
+@pytest.mark.parametrize("fstring,features,length", [astuple(case) for case in cases])
+def test_features_from_fstring(fstring: str, features: str, length: int):
+    fs = Features(fstring)
+    assert len(fs) == length
+    assert dict(fs) == features
+    assert fs.get("dummy") is None
+
+
+@pytest.mark.parametrize("fstring,features,length", [astuple(case) for case in cases])
+def test_features_to_fstring(fstring: str, features: str, length: int):
+    fs = Features(fstring)
+    assert fs.to_fstring() == fstring
