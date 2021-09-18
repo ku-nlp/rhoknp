f79ad0b90da6f542b442c62fc07b275ebbc35a2d
nobu-g <ueda-n127@hotmail.co.jp>
2021-09-18 12:30:26 +0900

    store parent node as weakref to avoid circular reference

--- src/rhoknp/units/unit.py
+++ src/rhoknp/units/unit.py
@@ -1,9 +1,10 @@
 from typing import Optional
+import weakref
 
 
 class Unit:
     def __init__(self, parent_unit: Optional["Unit"]):
-        self.parent_unit = parent_unit
+        self.parent_unit = parent_unit and weakref.ref(parent_unit)
 
     @property
     def child_units(self) -> Optional[list["Unit"]]:
