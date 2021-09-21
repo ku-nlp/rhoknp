b906dcd8789b11409f1cdaa374bce1982f8b35d2
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2021-09-21 12:27:22 +0900

    remove unused files

--- src/rhoknp/__init__.py
+++ src/rhoknp/__init__.py
@@ -1,7 +1,5 @@
 from importlib.metadata import version
 
-from .rhoknp import load_jumanpp, load_knp, parse
-
 __version__ = version("rhoknp")
 
-__all__ = ["__version__", "parse", "load_jumanpp", "load_knp"]
+__all__ = ["__version__"]
--- src/rhoknp/utils/utils.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-
-class Format(enum.Enum):
-    JUMANPP = enum.auto()
-    KNP = enum.auto()
