4eb8ebf90214b890eb484c144fb0c0c84829ad7f
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2021-10-15 15:42:50 +0900

    document Jumanpp and KNP

--- src/rhoknp/processors/jumanpp.py
+++ src/rhoknp/processors/jumanpp.py
@@ -22,15 +22,39 @@ class Jumanpp(Processor):
         Args:
             executable: Juman++ のパス．
             options: Juman++ のオプション．
-            senter: 文分割器のインスタンス．文分割がまだなら，先にこのインスタンスを用いて文分割を行う．
-                未設定の場合， RegexSenter が適用される．
+            senter: 文分割器のインスタンス．文分割がまだなら，先にこのインスタンスを用いて文分割する．
+                未設定なら RegexSenter を使って文分割する．
+
+        Example:
+
+            from rhoknp import Jumanpp
+
+            jumanpp = Jumanpp()
+            sentence = jumanpp.apply("電気抵抗率は、どんな材料が電気を通しにくいかを比較するために、用いられる物性値である。")
         """
         self.executable = executable
         self.options = options
 
         self.senter = senter
 
+    def __repr__(self) -> str:
+        arg_string = f"executable={repr(self.executable)}"
+        if self.options is not None:
+            arg_string += f", options={repr(self.options)}"
+        if self.senter is not None:
+            arg_string += f", senter={repr(self.senter)}"
+        return f"{self.__class__.__name__}({arg_string})"
+
     def apply_to_document(self, document: Union[Document, str]) -> Document:
+        """文書に Jumanpp を適用する．
+
+        Args:
+            document: 文書．
+
+        .. note::
+            文分割がまだなら，先に初期化時に設定した senter で文分割する．
+            未設定なら RegexSenter で文分割する．
+        """
         if isinstance(document, str):
             document = Document(document)
 
@@ -48,6 +72,11 @@ class Jumanpp(Processor):
         return Document.from_jumanpp(jumanpp_text)
 
     def apply_to_sentence(self, sentence: Union[Sentence, str]) -> Sentence:
+        """文に Jumanpp を適用する．
+
+        Args:
+            sentence: 文．
+        """
         if isinstance(sentence, str):
             sentence = Sentence(sentence)
 
@@ -56,6 +85,7 @@ class Jumanpp(Processor):
         return Sentence.from_jumanpp(jumanpp_text)
 
     def is_available(self) -> bool:
+        """Jumanpp が利用可能であれば True を返す．"""
         try:
             p = run(self.version_command, stdout=PIPE, stdin=PIPE, encoding="utf-8")
             logger.info(p.stdout.strip())
@@ -66,6 +96,7 @@ class Jumanpp(Processor):
 
     @property
     def run_command(self) -> list[str]:
+        """解析時に実行するコマンド．"""
         command = [self.executable]
         if self.options:
             command += self.options
@@ -73,12 +104,5 @@ class Jumanpp(Processor):
 
     @property
     def version_command(self) -> list[str]:
+        """バージョン確認時に実行するコマンド．"""
         return [self.executable, "-v"]
-
-    def __repr__(self) -> str:
-        arg_string = f"executable={repr(self.executable)}"
-        if self.options is not None:
-            arg_string += f", options={repr(self.options)}"
-        if self.senter is not None:
-            arg_string += f", senter={repr(self.senter)}"
-        return f"{self.__class__.__name__}({arg_string})"
