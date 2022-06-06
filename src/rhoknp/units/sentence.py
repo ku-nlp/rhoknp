49921a059d07f27195a72ef3673bbc753a0a32e8
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2022-06-06 22:15:06 +0900

    apply linters

--- src/rhoknp/units/sentence.py
+++ src/rhoknp/units/sentence.py
@@ -168,15 +168,11 @@ class Sentence(Unit):
             return self._morphemes
         elif self._clauses is not None:
             return [
-                morpheme
-                for clause in self.clauses
-                for morpheme in clause.morphemes
+                morpheme for clause in self.clauses for morpheme in clause.morphemes
             ]
         elif self._phrases is not None:
             return [
-                morpheme
-                for phrase in self.phrases
-                for morpheme in phrase.morphemes
+                morpheme for phrase in self.phrases for morpheme in phrase.morphemes
             ]
         raise AttributeError("not available before applying Jumanpp")
 
