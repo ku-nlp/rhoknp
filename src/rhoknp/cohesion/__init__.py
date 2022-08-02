99b5d3803981b835706234fbd4e557af97cc76d8
nobu-g <ueda-n127@hotmail.co.jp>
2022-08-02 19:57:48 +0900

    add exposed classes in cohesion package

--- src/rhoknp/cohesion/__init__.py
+++ src/rhoknp/cohesion/__init__.py
@@ -1,16 +1,24 @@
 from .argument import Argument, ArgumentType, SpecialArgument
-from .coreference import Entity
+from .coreference import Entity, EntityManager
+from .discourse import DiscourseRelation, DiscourseRelationList
 from .exophora import ExophoraReferent, ExophoraReferentType
 from .pas import Pas
 from .predicate import Predicate
+from .rel import Rel, RelList, RelMode
 
 __all__ = [
+    "Pas",
     "Predicate",
     "ArgumentType",
     "Argument",
     "SpecialArgument",
-    "Pas",
     "ExophoraReferent",
     "ExophoraReferentType",
     "Entity",
+    "EntityManager",
+    "Rel",
+    "RelList",
+    "RelMode",
+    "DiscourseRelation",
+    "DiscourseRelationList",
 ]
