9a4b4202e087007e4ce425410df9f0111d636221
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2022-09-14 18:11:11 +0900

    test show command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,9 @@
+import tempfile
+import textwrap
+
 from typer.testing import CliRunner
 
-from rhoknp import __version__
+from rhoknp import Sentence, __version__
 from rhoknp.cli import app
 
 runner = CliRunner()
@@ -10,3 +13,36 @@ def test_version():
     result = runner.invoke(app, ["-v"])
     assert result.exit_code == 0
     assert result.stdout.strip() == f"rhoknp version: {__version__}"
+
+
+def test_show():
+    knp = textwrap.dedent(
+        """\
+        # S-ID:1
+        * 1D
+        + 1D
+        望遠 ぼうえん 望遠 名詞 6 普通名詞 1 * 0 * 0 "代表表記:望遠/ぼうえん カテゴリ:抽象物"
+        + 2D
+        鏡 きょう 鏡 名詞 6 普通名詞 1 * 0 * 0 "代表表記:鏡/きょう カテゴリ:人工物-その他 漢字読み:音"
+        で で で 助詞 9 格助詞 1 * 0 * 0 NIL
+        * 2D
+        + 3D
+        泳いで およいで 泳ぐ 動詞 2 * 0 子音動詞ガ行 4 タ系連用テ形 14 "代表表記:泳ぐ/およぐ"
+        いる いる いる 接尾辞 14 動詞性接尾辞 7 母音動詞 1 基本形 2 "代表表記:いる/いる"
+        * 3D
+        + 4D
+        少女 しょうじょ 少女 名詞 6 普通名詞 1 * 0 * 0 "代表表記:少女/しょうじょ カテゴリ:人"
+        を を を 助詞 9 格助詞 1 * 0 * 0 NIL
+        * -1D
+        + -1D
+        見た みた 見る 動詞 2 * 0 母音動詞 1 タ形 10 "代表表記:見る/みる 自他動詞:自:見える/みえる 補文ト"
+        。 。 。 特殊 1 句点 1 * 0 * 0 NIL
+        EOS
+        """
+    )
+    sentence = Sentence.from_knp(knp)
+    with tempfile.NamedTemporaryFile("wt") as f:
+        f.write(sentence.to_knp())
+        f.flush()
+        result = runner.invoke(app, ["show", f.name])
+        assert result.exit_code == 0
