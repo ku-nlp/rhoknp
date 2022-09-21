f47d04422254b82eefe65a00cf87eebdf72617d4
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2022-09-21 10:56:50 +0900

    add reader.py

---
+++ src/rhoknp/utils/reader.py
@@ -0,0 +1,105 @@
+import logging
+import re
+from typing import IO, Callable, Iterator, List, Literal, Optional, Union
+
+from rhoknp import Sentence
+
+logger = logging.getLogger(__name__)
+
+
+class Reader:
+    """言語解析結果を読み込むクラス．"""
+
+    def __init__(self, f: IO) -> None:
+        self.f = f
+
+    def __call__(self) -> Iterator[str]:
+        return self.read_as_sentences()
+
+    def read_as_sentences(self) -> Iterator[str]:
+        """ファイルを文に対する解析結果の集合として読み込む．
+
+        Examples:
+
+            >>> from rhoknp.units import Sentence
+            >>> from rhoknp.utils.reader import Reader
+            <BLANKLINE>
+            >>> with open("example.knp") as f:
+            ...     reader = Reader(f)
+            ...     for knp in reader.read_as_sentences():
+            ...         sentence = Sentence.from_knp(knp)
+        """
+        buffer = []
+        for line in self.f:
+            if line.strip() == "":
+                continue
+            buffer.append(line)
+            if line.strip() == Sentence.EOS_PAT:
+                yield "".join(buffer)
+                buffer = []
+        if buffer:
+            yield "".join(buffer)
+
+    def read_as_documents(
+        self, doc_id_format: Union[Literal["default", "kwdlc", "wac"], Callable] = "default"
+    ) -> Iterator[str]:
+        """ファイルを文書に対する解析結果の集合として読み込む．
+
+        Args:
+            doc_id_format: 文書IDのフォーマット．
+
+        Examples:
+
+            >>> from rhoknp.units import Document
+            >>> from rhoknp.utils.reader import Reader
+            <BLANKLINE>
+            >>> with open("example.knp") as f:
+            ...     reader = Reader(f)
+            ...     for knp in reader.read_as_documents():
+            ...         document = Document.from_knp(knp)
+        """
+        if isinstance(doc_id_format, str):
+            if doc_id_format == "default":
+                extract_doc_id = _extract_doc_id(Sentence.SID_PAT)
+            elif doc_id_format == "kwdlc":
+                extract_doc_id = _extract_doc_id(Sentence.SID_PAT_KWDLC)
+            elif doc_id_format == "wac":
+                extract_doc_id = _extract_doc_id(Sentence.SID_PAT_WAC)
+            else:
+                raise ValueError(f"Invalid doc_id_format: {doc_id_format}")
+        elif callable(doc_id_format):
+            extract_doc_id = doc_id_format
+        else:
+            raise ValueError(f"Invalid doc_id_format: {doc_id_format}")
+
+        prev_doc_id: Optional[str] = None
+        buffer: List[str] = []
+        for sentence in self.read_as_sentences():
+            doc_id = extract_doc_id(sentence.split("\n")[0])
+            if prev_doc_id is not None and prev_doc_id != doc_id:
+                yield "".join(buffer)
+                buffer = []
+            buffer.append(sentence)
+            prev_doc_id = doc_id
+        if buffer:
+            yield "".join(buffer)
+
+
+def _extract_doc_id(pat: re.Pattern) -> Callable[[str], Optional[str]]:
+    """文書IDを抽出する関数を返す．
+
+    Args:
+        pat: 文書IDを抽出する正規表現．
+    """
+
+    def extract_doc_id(line: str) -> Optional[str]:
+        if match_sid := re.match(r"# S-ID: ?(\S*)( .+)?$", line):
+            sid_string = match_sid.group(1)
+            match = pat.match(sid_string)
+            if match is None:
+                logger.warning(f"Invalid S-ID: {sid_string}")
+                return None
+            return match.group("did")
+        return None
+
+    return extract_doc_id
---
+++ tests/utils/test_reader.py
@@ -0,0 +1,114 @@
+import re
+import textwrap
+from io import StringIO
+from typing import Any, Dict, Optional
+
+import pytest
+
+from rhoknp import Sentence
+from rhoknp.utils.reader import Reader, _extract_doc_id
+
+CASES = [
+    {
+        "text": textwrap.dedent(
+            """\
+            # S-ID:1-1
+            EOS
+            # S-ID:1-2
+            EOS
+            """
+        ),
+        "sentences": [
+            "# S-ID:1-1\nEOS\n",
+            "# S-ID:1-2\nEOS\n",
+        ],
+        "documents": [
+            "# S-ID:1-1\nEOS\n# S-ID:1-2\nEOS\n",
+        ],
+        "doc_id_format": "default",
+    },
+    {
+        "text": textwrap.dedent(
+            """\
+            # S-ID:w201106-0000060050-1
+            EOS
+            # S-ID:w201106-0000060050-2
+            EOS
+            """
+        ),
+        "sentences": [
+            "# S-ID:w201106-0000060050-1\nEOS\n",
+            "# S-ID:w201106-0000060050-2\nEOS\n",
+        ],
+        "documents": [
+            "# S-ID:w201106-0000060050-1\nEOS\n# S-ID:w201106-0000060050-2\nEOS\n",
+        ],
+        "doc_id_format": "kwdlc",
+    },
+    {
+        "text": textwrap.dedent(
+            """\
+            # S-ID:wiki00100176-00
+            EOS
+            # S-ID:wiki00100176-01
+            EOS
+            """
+        ),
+        "sentences": [
+            "# S-ID:wiki00100176-00\nEOS\n",
+            "# S-ID:wiki00100176-01\nEOS\n",
+        ],
+        "documents": [
+            "# S-ID:wiki00100176-00\nEOS\n# S-ID:wiki00100176-01\nEOS\n",
+        ],
+        "doc_id_format": "wac",
+    },
+    {
+        "text": textwrap.dedent(
+            """\
+            # S-ID:1-1
+            EOS
+            # S-ID:1-2
+            EOS
+            """
+        ),
+        "sentences": [
+            "# S-ID:1-1\nEOS\n",
+            "# S-ID:1-2\nEOS\n",
+        ],
+        "documents": [
+            "# S-ID:1-1\nEOS\n# S-ID:1-2\nEOS\n",
+        ],
+        "doc_id_format": lambda x: x.split("-")[0],
+    },
+]
+
+
+@pytest.mark.parametrize("case", CASES)
+def test_read_as_sentences(case: Dict[str, Any]) -> None:
+    reader = Reader(StringIO(case["text"]))
+    actual = list(reader.read_as_sentences())
+    assert actual == case["sentences"]
+
+
+@pytest.mark.parametrize("case", CASES)
+def test_read_as_documents(case: Dict[str, Any]) -> None:
+    reader = Reader(StringIO(case["text"]))
+    actual = list(reader.read_as_documents(doc_id_format=case["doc_id_format"]))
+    assert actual == case["documents"]
+
+
+@pytest.mark.parametrize(
+    "pat, line, doc_id",
+    [
+        (Sentence.SID_PAT, "# S-ID:1", "1"),
+        (Sentence.SID_PAT, "# S-ID:1-1", "1"),
+        (Sentence.SID_PAT, "# S-ID:1-2", "1"),
+        (Sentence.SID_PAT, "# S-ID:a-1", "a"),
+        (Sentence.SID_PAT, "# S-ID:a-2", "a"),
+        (Sentence.SID_PAT_KWDLC, "# S-ID:w201106-0000060050-1", "w201106-0000060050"),
+        (Sentence.SID_PAT_WAC, "# S-ID:wiki00100176-00", "wiki00100176"),
+    ],
+)
+def test_extract_doc_id(pat: re.Pattern, line: str, doc_id: Optional[str]) -> None:
+    assert _extract_doc_id(pat)(line) == doc_id
