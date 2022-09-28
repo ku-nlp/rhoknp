66f88bb6beba9f8a90e13038e6c627f77680cef3
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2022-09-28 21:11:57 +0900

    add KWJA processor

--- src/rhoknp/__init__.py
+++ src/rhoknp/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version
 
-from .processors import KNP, Jumanpp, RegexSenter
+from .processors import KNP, KWJA, Jumanpp, RegexSenter
 from .units import BasePhrase, Clause, Document, Morpheme, Phrase, Sentence
 
 __version__ = version("rhoknp")
@@ -10,6 +10,7 @@ __all__ = [
     "RegexSenter",
     "Jumanpp",
     "KNP",
+    "KWJA",
     "Document",
     "Sentence",
     "Clause",
--- src/rhoknp/processors/__init__.py
+++ src/rhoknp/processors/__init__.py
@@ -1,5 +1,6 @@
 from .jumanpp import Jumanpp
 from .knp import KNP
+from .kwja import KWJA
 from .senter import RegexSenter
 
-__all__ = ["RegexSenter", "Jumanpp", "KNP"]
+__all__ = ["RegexSenter", "Jumanpp", "KNP", "KWJA"]
