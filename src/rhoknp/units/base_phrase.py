30d3ddd55df569a88168567054c59ad0b0735b3e
nobu-g <ueda-n127@hotmail.co.jp>
2023-01-31 12:49:18 +0900

    support global_index for units without document

--- src/rhoknp/units/base_phrase.py
+++ src/rhoknp/units/base_phrase.py
@@ -106,6 +106,8 @@ class BasePhrase(Unit):
     @cached_property
     def global_index(self) -> int:
         """文書全体におけるインデックス．"""
+        if self.sentence.has_document is False:
+            return self.index
         if self.index > 0:
             return self.sentence.base_phrases[self.index - 1].global_index + 1
         if self.sentence.index == 0:
