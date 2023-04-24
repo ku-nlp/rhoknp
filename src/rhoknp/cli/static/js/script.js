521f35fef2fda6569a0f7f9e5c0acd7587ced8d7
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-04-24 14:26:18 +0900

    add show/hide-all button

--- src/rhoknp/cli/static/js/script.js
+++ src/rhoknp/cli/static/js/script.js
@@ -20,3 +20,23 @@ accordionItems.forEach((item) => {
         item.querySelector(".accordion-collapse").classList.add("show")
     }
 })
+
+const showAllButton = document.querySelector("#show-all-button");
+
+showAllButton.addEventListener("click", () => {
+    accordionItems.forEach((item) => {
+        localStorage.setItem("accordion-" + item.id, "true")
+        item.querySelector(".accordion-button").classList.remove("collapsed")
+        item.querySelector(".accordion-collapse").classList.add("show")
+    })
+})
+
+const hideAllButton = document.querySelector("#hide-all-button");
+
+hideAllButton.addEventListener("click", () => {
+    accordionItems.forEach((item) => {
+        localStorage.removeItem("accordion-" + item.id)
+        item.querySelector(".accordion-button").classList.add("collapsed")
+        item.querySelector(".accordion-collapse").classList.remove("show")
+    })
+})
