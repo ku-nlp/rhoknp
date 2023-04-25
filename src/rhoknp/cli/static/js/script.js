efca564948c95ef8ea2005f1a108542a1bace2bf
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-04-25 12:02:47 +0900

    open the result of some language analysis by default

--- src/rhoknp/cli/static/js/script.js
+++ src/rhoknp/cli/static/js/script.js
@@ -1,42 +1,52 @@
 /* Keep the status of the accordion to show analysis results */
-const accordionItems = document.querySelectorAll(".accordion-item");
-
-accordionItems.forEach((item) => {
-    item.addEventListener("shown.bs.collapse", () => {
-        localStorage.setItem("accordion-" + item.id, "true")
-    })
-})
-
-accordionItems.forEach((item) => {
-    item.addEventListener("hidden.bs.collapse", () => {
-        localStorage.removeItem("accordion-" + item.id)
-    })
-})
-
-accordionItems.forEach((item) => {
-    const state = localStorage.getItem("accordion-" + item.id)
-    if (state === "true") {
-        item.querySelector(".accordion-button").classList.remove("collapsed")
-        item.querySelector(".accordion-collapse").classList.add("show")
+const defaultOpenAccordionItems = document.querySelectorAll(".accordion-item-default-open");
+defaultOpenAccordionItems.forEach((item) => {
+    const itemId = `accordion-${item.id}`;
+    if (localStorage.getItem(itemId) === null) {
+        localStorage.setItem(itemId, "true");
     }
-})
+});
+
+const accordionItems = document.querySelectorAll(".accordion-item");
+accordionItems.forEach((item) => {
+    const itemId = `accordion-${item.id}`;
+    item.addEventListener("shown.bs.collapse", () => {
+        localStorage.setItem(itemId, "true");
+    });
+    item.addEventListener("hidden.bs.collapse", () => {
+        localStorage.setItem(itemId, "false");
+    });
+});
+
+accordionItems.forEach((item) => {
+    const itemId = `accordion-${item.id}`;
+    const state = localStorage.getItem(itemId);
+    console.log(state);
+    if (state === "true") {
+        item.querySelector(".accordion-button").classList.remove("collapsed");
+        item.querySelector(".accordion-collapse").classList.add("show");
+    } else {
+        item.querySelector(".accordion-button").classList.add("collapsed");
+        item.querySelector(".accordion-collapse").classList.remove("show");
+    }
+});
 
 const showAllButton = document.querySelector("#show-all-button");
-
 showAllButton.addEventListener("click", () => {
     accordionItems.forEach((item) => {
-        localStorage.setItem("accordion-" + item.id, "true")
-        item.querySelector(".accordion-button").classList.remove("collapsed")
-        item.querySelector(".accordion-collapse").classList.add("show")
+        const itemId = `accordion-${item.id}`;
+        localStorage.setItem(itemId, "true");
+        item.querySelector(".accordion-button").classList.remove("collapsed");
+        item.querySelector(".accordion-collapse").classList.add("show");
     })
 })
 
 const hideAllButton = document.querySelector("#hide-all-button");
-
 hideAllButton.addEventListener("click", () => {
     accordionItems.forEach((item) => {
-        localStorage.removeItem("accordion-" + item.id)
-        item.querySelector(".accordion-button").classList.add("collapsed")
-        item.querySelector(".accordion-collapse").classList.remove("show")
+        const itemId = `accordion-${item.id}`;
+        localStorage.setItem(itemId, "false");
+        item.querySelector(".accordion-button").classList.add("collapsed");
+        item.querySelector(".accordion-collapse").classList.remove("show");
     })
 })
