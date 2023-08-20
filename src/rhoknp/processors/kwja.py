3b3f260c7b0a155afad2ff0e8f32c358a70d7e78
nobu-g <ueda-n127@hotmail.co.jp>
2023-08-20 20:46:19 +0900

    tweak

--- src/rhoknp/processors/kwja.py
+++ src/rhoknp/processors/kwja.py
@@ -55,7 +55,7 @@ class KWJA(Processor):
         try:
             self._proc = Popen(self.run_command, stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding="utf-8")
             if skip_sanity_check is False:
-                # TODO: replace "あ" with an empty string after KWJA v2.2.0 is released
+                # TODO: replace "こんにちは" with an empty string after KWJA v2.2.0 is released
                 _ = self.apply(Document.from_raw_text("こんにちは"))
         except Exception as e:
             logger.warning(f"failed to start KWJA: {e}")
