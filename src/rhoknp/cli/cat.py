5799df654dff0f20eac921d432e410b3e31b7726
nobu-g <ueda-n127@hotmail.co.jp>
2023-11-05 00:07:05 +0900

    fix pygments lexer definition

--- src/rhoknp/cli/cat.py
+++ src/rhoknp/cli/cat.py
@@ -3,10 +3,9 @@ from typing import ClassVar, List
 from pygments import highlight
 from pygments.formatters import TerminalFormatter
 from pygments.lexer import RegexLexer
-from pygments.token import Comment, Generic, Keyword, Text
+from pygments.token import Comment, Keyword, Number, Operator, String, Text
 
 from rhoknp import Document
-from rhoknp.units import BasePhrase, Morpheme, Phrase
 
 
 class KNPLexer(RegexLexer):
@@ -18,12 +17,50 @@ class KNPLexer(RegexLexer):
     mimetypes: ClassVar[List[str]] = ["text/plain"]
 
     tokens = {  # noqa: RUF012
+        # "root": [
+        #     (Phrase.PAT.pattern, Generic.Heading),
+        #     (BasePhrase.PAT.pattern, Generic.Subheading),
+        #     (Morpheme.PAT.pattern, Text),
+        #     (r"^#.*$", Comment.Single),
+        #     (r"^EOS$", Keyword.Reserved),
+        # ],
         "root": [
-            (Phrase.PAT.pattern, Generic.Heading),
-            (BasePhrase.PAT.pattern, Generic.Subheading),
-            (Morpheme.PAT.pattern, Text),
-            (r"^#.*$", Comment.Single),
-            (r"^EOS$", Keyword.Reserved),
+            # Keywords
+            (r"<", Keyword, "feature"),
+            (r"^\+", Keyword, "tag_bnst"),
+            (r"^\*", Keyword, "tag_bnst"),
+            # EOS constant
+            (r"^EOS$", Keyword.Constant),
+            # Strings
+            (r"\"", String, "string"),
+            # Comments
+            (r"^#", Comment, "comment"),
+            # Other text
+            (r"^[^+*\#\"<> ]+", Text),
+            (r"(?<=\s)[^+*\#\"<> ]+", Text),
+            (r".", Text),
+        ],
+        "string": [
+            (r'[^"]+', String),
+            (r"\"", String, "#pop"),
+        ],
+        "comment": [
+            (r"[^\n]+", Comment),
+            (r"\n", Text, "#pop"),
+        ],
+        "tag_bnst": [
+            (r"(-1|\d+)[DPAI]", Number.Integer, "#pop"),
+            (r"<", Keyword, "feature"),
+            (r"\n", Text, "#pop"),
+        ],
+        "feature": [
+            (r"\"", String, "string"),
+            (r">", Keyword, "#pop"),
+            (r":", Keyword),
+            (r"=", Operator),
+            # Feature value context is not yet defined in the .sublime-syntax sample provided
+            # Assuming it could be similar to 'string' context for now
+            (r'[^\s=>"]+', Text),  # Catch all for other text within a feature
         ],
     }
 
