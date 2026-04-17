29e926025171736aebf9b65c01c60103304caffd
nobu-g <ueda-n127@hotmail.co.jp>
2026-04-17 18:08:00 +0900

    add pragma: no cover

--- src/rhoknp/_compat.py
+++ src/rhoknp/_compat.py
@@ -1,8 +1,8 @@
 import sys
 
 if sys.version_info >= (3, 12):
-    from typing import override
+    from typing import override  # pragma: no cover
 else:
-    from typing_extensions import override
+    from typing_extensions import override  # pragma: no cover
 
 __all__ = ["override"]
