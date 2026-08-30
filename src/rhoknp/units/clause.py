52cfae7f45bc11fea63978db69d5c2b9bff627ec
nobu-g <ueda-n127@hotmail.co.jp>
2026-08-30 18:18:56 +0900

    update pre-commit hooks

--- src/rhoknp/units/clause.py
+++ src/rhoknp/units/clause.py
@@ -176,7 +176,7 @@ class Clause(Unit):
     def parent(self) -> Optional["Clause"]:
         """係り先の節．ないなら None．"""
         head_parent = self.head.parent
-        while head_parent in self.base_phrases:
+        while head_parent is not None and head_parent in self.base_phrases:
             head_parent = head_parent.parent
         for clause in self.sentence.clauses:
             if head_parent in clause.base_phrases:
